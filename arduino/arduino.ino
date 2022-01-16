3297f2310bed5d755f56fbb139b16f56a608c534
Przemyk <przemek@przemyk.xyz>
2022-01-16 14:49:55 +0100

    arduino nano tweaks

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,13 +1,12 @@
 #include "../common.h"
 
+BATTERY_DATA_T charge_now = 0; // TODO: update it and store periodically on EEPROM
+BATTERY_DATA_T voltage_now = 15000000;
+BATTERY_DATA_T current_now = 3000000;
+
 void setup() {
     Serial.begin(9600);
 }
-
-BATTERY_DATA_T charge_now = 3000000;
-BATTERY_DATA_T charge_counter = 1000000;
-BATTERY_DATA_T voltage_now = 15000000;
-BATTERY_DATA_T current_now = 3000000;
 
 void writeData(BATTERY_DATA_T data) {
     byte buf[sizeof(BATTERY_DATA_T)];
@@ -20,7 +19,15 @@ void writeData(BATTERY_DATA_T data) {
 }
 
 void loop() {
-    current_now = ((analogRead(A0) * (5.0 / 1023.0) - 2.5) / 185.0 / 4.8 * 5.0 * 1000000000.0);
+    // arduino nano A5 & A7 ports
+
+    // leonardo
+    //voltage_now = (analogRead(A5) * (5.0 / 1023.0) * 4.058) * 1000000;
+
+    // nano needs different scale. I've spent too much time trying to find the best scale...
+    voltage_now = (analogRead(A5) * (5.0 / 1023.0) * 3.7) * 1000000;
+    current_now = ((analogRead(A7) * (5.0 / 1023.0) - 2.5) / 185.0 / 4.8 * 5.0 * 1000000000.0);
+    current_now = abs(current_now);
 
     while (Serial.available() > 0) {
         char command = Serial.read();
@@ -29,8 +36,8 @@ void loop() {
         case BATTERY_CMD_CHARGE_NOW:
             writeData(charge_now);
             break;
-        case BATTERY_CMD_CHARGE_COUNTER:
-            writeData(charge_counter);
+        case BATTERY_CMD_CHARGE_COUNTER: //TODO: remove I think
+            writeData(charge_now);
             break;
         case BATTERY_CMD_VOLTAGE_NOW:
             writeData(voltage_now);
